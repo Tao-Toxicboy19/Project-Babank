75764ca8ccf6f29141992d5763d1935615188b90
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-08-11 21:11:55 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Link, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./components/Pages/LoginPage/LoginPage";
+import RegisterPage from "./components/Pages/RegisterPage/RegisterPage";
+import PlotdataPage from "./components/Pages/PlotdataPage/PlotdataPage";
+import MachinePage from "./components/Pages/MachinePage/MachinePage";
+import MachineMovementPage from "./components/Pages/MachineMovementPage/MachineMovementPage";
+import ContactPage from "./components/Pages/ContactPage/ContactPage";
+import HomePage from "./components/Pages/HomePage/HomePage";
+import Header from "./components/layout/Header/Header";
+
+type Props = {};
+
+export default function App({}: Props) {
+  const [data, setData] = useState(0);
+
+  return (
+    <div>
+      <Header />
+      <hr />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/plotdata" element={<PlotdataPage />} />
+        <Route path="/machineinformation" element={<MachinePage />} />
+        <Route path="/machinemovement" element={<MachineMovementPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </div>
+  );
+}
---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,124 @@
+import { Link } from "react-router-dom";
+import { AiFillHome, AiOutlineForm } from "react-icons/ai";
+import { PiPlantFill } from "react-icons/pi";
+import { FiLogIn } from "react-icons/fi";
+import { FaTractor, FaRoute } from "react-icons/fa";
+import { RiContactsBookFill } from "react-icons/ri";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to={"/"}>
+                <AiFillHome />
+                หน้าแรก
+              </Link>
+            </li>
+            <li>
+              <Link to={"/plotdata"}>
+                <PiPlantFill />
+                ข้อมูลแปลง
+              </Link>
+            </li>
+            <li>
+              <Link className="" to={"/machineinformation"}>
+                <FaTractor />
+                ข้อมูลเครื่องจักร
+              </Link>
+            </li>
+            <li>
+              <Link to={"/machinemovement"}>
+                <FaRoute />
+                เส้นทางการเคลื่อนย้ายของเครื่องจัก
+              </Link>
+            </li>
+            <li>
+              <Link to={"/contact"}>
+                <RiContactsBookFill />
+                ติอต่อเรา
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to={"/"}>
+              <AiFillHome />
+              หน้าแรก
+            </Link>
+          </li>
+          <li>
+            <Link to={"/plotdata"}>
+              <PiPlantFill />
+              ข้อมูลแปลง
+            </Link>
+          </li>
+          <li>
+            <Link className="" to={"/machineinformation"}>
+              <FaTractor />
+              ข้อมูลเครื่องจักร
+            </Link>
+          </li>
+          <li>
+            <Link to={"/machinemovement"}>
+              <FaRoute />
+              เส้นทางการเคลื่อนย้ายของเครื่องจัก
+            </Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>
+              <RiContactsBookFill />
+              ติอต่อเรา
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <ul className="flex justify-center gap-x-5 mx-3">
+          <li>
+            <Link
+              to={"/register"}
+              className="flex flex-row items-center gap-x-2"
+            >
+              <AiOutlineForm />
+              Regiset
+            </Link>
+          </li>
+          <li>
+            <Link to={"/login"} className="flex flex-row items-center gap-x-2">
+              <FiLogIn />
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
