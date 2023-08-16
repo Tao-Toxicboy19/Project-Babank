6cd87f22c0ecbd1d574eabf8fdbdd6743d48128a
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-08-16 23:33:46 +0700

    test

--- src/components/Pages/CarrierPage/CarrierPage.tsx
+++ src/components/Pages/CarrierPage/CarrierPage.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-type Props = {}
-
-export default function CarrierPage({}: Props) {
-  return (
-    <div>CarrierPage</div>
-  )
+export interface carrier {
+  id: number;
+  name: string;
+  maxcapacity: number;
+  power: string;
+  burden: number;
 }
+
+const CarrierPage = () => {
+  const [carriers, setCarriers] = useState([]);
+
+  useEffect(() => {
+    async function fetchCarriers() {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/getCarriers"
+        );
+        setCarriers(response.data.Carriers);
+      } catch (error) {
+        console.error("Error fetching carriers:", error);
+      }
+    }
+
+    fetchCarriers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Carrier Page</h1>
+      {carriers.map((carrier: carrier) => (
+        <ul key={carrier.id}>
+          <li>{carrier.id}</li>
+          <li>{carrier.maxcapacity}</li>
+          <li>{carrier.power}</li>
+          <li>{carrier.burden}</li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+export default CarrierPage;
