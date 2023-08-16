586cc6aa26e0b12640a0bdaeee72fbfb28443284
artartzaza1123 <narongsaktanan@gmail.com>
2023-08-16 23:39:20 +0700

    testCarrier

--- src/components/Pages/CarrierPage/CarrierPage.tsx
+++ src/components/Pages/CarrierPage/CarrierPage.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-type Props = {}
+type Props = {};
+
+interface Carrier {
+  // Define the structure of your Carrier object here
+  id: number;
+  name: string;
+  // ... other properties
+}
 
 export default function CarrierPage({}: Props) {
+  const [carriers, setCarriers] = useState<Carrier[]>([]);
+
+  useEffect(() => {
+    // Fetch data from the API using axios when the component mounts
+    axios
+      .get("http://localhost:8080/api/getCarriers")
+      .then((response) => {
+        setCarriers(response.data.Carriers);
+        console.log(response.data.Carriers);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []); // Empty dependency array means this effect runs only once on mount
+
   return (
-    <div>CarrierPage</div>
-  )
+    <div>
+      <h1>CarrierPage</h1>
+      <ul>
+        {carriers.map((carrier) => (
+          <li key={carrier.id}>{carrier.name}</li>
+          // Render other carrier properties as needed
+        ))}
+      </ul>
+    </div>
+  );
 }
